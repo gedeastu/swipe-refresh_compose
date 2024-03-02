6bc5917e5ac499dac8d816f5ab68417c47bc62bb
Gede Astu Nugraha <gede.astu@gmail.com>
2024-03-02 13:56:08 +0700

    Setup

---
+++ app/src/main/java/com/example/swiperefresh/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.swiperefresh
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.swiperefresh.ui.theme.SwipeRefreshTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SwipeRefreshTheme {
+                // A surface container using the 'background' color from the theme
+                val
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SwipeRefreshTheme {
+        Greeting("Android")
+    }
+}
