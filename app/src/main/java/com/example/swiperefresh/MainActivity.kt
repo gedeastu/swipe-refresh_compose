0b63eae4386bbf5f9144cba433a76708ad5ed802
Gede Astu Nugraha <gede.astu@gmail.com>
2024-03-02 14:48:49 +0700

    swipe and refresh is done

--- app/src/main/java/com/example/swiperefresh/MainActivity.kt
+++ app/src/main/java/com/example/swiperefresh/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.swiperefresh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.swiperefresh.presentation.MainViewModel
 import com.example.swiperefresh.ui.theme.SwipeRefreshTheme
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshIndicator
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +28,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             SwipeRefreshTheme {
                 // A surface container using the 'background' color from the theme
-                val
+                val viewModel = viewModel<MainViewModel>()
+                val isLoading by viewModel.isLoading.collectAsState()
+                val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = isLoading)
+                
+                SwipeRefresh(state = swipeRefreshState, onRefresh = viewModel::loadStuff, indicator = { state, refreshTrigger ->
+                    SwipeRefreshIndicator(
+                        state = state,
+                        refreshTriggerDistance = refreshTrigger,
+                        backgroundColor = Color.DarkGray,
+                        contentColor = Color.White
+                    )
+                }) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        content = {
+                        items(count = 100){
+                            Text(text = "Text $it", modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(32.dp))
+                        }
+                    })
+                }
             }
         }
     }
